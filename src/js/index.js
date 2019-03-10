f5d37eeab9ed1f75f5a02231c6a7d12b0af1789d
Oralinge <oralinge@163.com>
2019-03-10 17:56:37 +0800

    轮播图以上完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import { Z_ASCII } from "zlib";
 
 
 var $backButton=$('.back-to-top'); 
@@ -45,6 +46,242 @@ $('.logo').on('click',function(){
 });
 
 
-// 即时搜索
-	
+/*---------jq轮播图start----------*/
+
+
+    var timer = setInterval(auto,5000);
+    var index = 0;
+    function auto(){
+        index++;
+        if( index == $("ol li").size() ){
+            index = 0;
+        }
+        $("ol li").eq(index).addClass("current").siblings().removeClass("current");
+        $(".shuffling-list li ").eq(index).fadeIn(1000).siblings().fadeOut(800);
+    }
+    
+    //鼠标 操作
+    $("ol li").mouseenter(function(){
+        clearInterval(timer);
+        index = $(this).index()-1;
+        
+        auto();
+    });
+    $("ol li").mouseout(function(){
+        timer = setInterval(auto,5000);
+    })
+    
+    /*--------jq轮播图end-------*/
    
+   
+
+
+
+//     .select-menu:hover .CNY{
+//         display: block;
+//         z-index: 10;
+//    }
+   
+  /*------------CNY特效start-------------*/
+  $(function(){
+    $(".CNY").hide();
+    var _timeout;
+    $('.select-menu').hover(function(){
+        clearTimeout(_timeout);
+        $('.CNY').fadeIn(250);
+    },function(){
+       _timeout=setTimeout(function(){
+            $('.CNY').fadeOut(250);
+        },100)
+    });
+     
+    //内容也做hover事件
+    $(".CNY").hover(function(){
+        clearTimeout(_timeout);
+        $('.CNY').css("display","block");
+    },function(){
+        
+        $('.CNY').fadeOut(250);
+        
+    });
+     
+});
+
+
+   
+   
+  /*------------CNY特效end------------*/
+
+
+
+
+/*------------language特效start-------------*/
+$(function(){
+    $(".language").hide();
+    var _timeout;
+    $('.language-menu').hover(function(){
+        clearTimeout(_timeout);
+        $('.language').fadeIn(250);
+    },function(){
+       _timeout=setTimeout(function(){
+            $('.language').fadeOut(250);
+        },100)
+    });
+     
+    //内容也做hover事件
+    $(".language").hover(function(){
+        clearTimeout(_timeout);
+        $('.language').css("display","block");
+    },function(){
+        
+        $('.language').fadeOut(250);
+        
+    });
+     
+});
+
+
+/*------------language特效end-------------*/
+
+  
+  /*-----------促销活动二级菜单start --------------*/
+  $(function(){
+    $(".Sales-promotion-activity").hide();
+    var _timeout;
+    $('.gift-indent').hover(function(){
+       
+       
+        $('.gift-indent').css("background","#fff");
+        $('.gift-indent a').css('color','#51216b');
+        $('.gift').css('background','url(../img/gift.jpg) no-repeat');
+       
+       
+        clearTimeout(_timeout);
+        $('.Sales-promotion-activity').slideDown(250);
+    },function(){
+       
+        _timeout=setTimeout(function(){
+            $('.Sales-promotion-activity').slideUp(250);
+            $('.gift-indent').css("background","#9f307c");
+            $('.gift-indent a').css({'color':'#fff','text-decoration': 'underline'});
+            $('.gift').css('background','url(../img/giftblue.jpg) no-repeat'); 
+        },100)
+    });
+     
+    //内容也做hover事件
+    $(".Sales-promotion-activity").hover(function(){
+        clearTimeout(_timeout);
+        $('.Sales-promotion-activity').css("display","block");
+        $('.gift-indent').css("background","#fff");
+        $('.gift-indent a').css('color','#51216b');
+        $('.gift').css('background','url(../img/gift.jpg) no-repeat');
+    },function(){
+       
+        $('.gift-indent').css("background","#9f307c");
+        $('.gift-indent a').css('color','#fff');
+        $('.gift').css('background','url(../img/giftblue.jpg) no-repeat');
+        _timeout=setTimeout(function(){
+            $('.Sales-promotion-activity').slideUp(250);
+        },100)
+    });
+     
+});
+   
+ /*-----------促销活动二级菜单end --------------*/
+
+
+//  .gift-indent:hover{
+//     background:#fff;
+//     a{
+//         color:#51216b;
+//         text-decoration: underline;
+//         .gift{
+//             background:url(../img/gift.jpg) no-repeat;
+//         }
+//     }
+// }
+
+/*----------- 品牌导航start --------------*/
+
+    
+    $(function(){
+        $(".brand").hide();
+        var _timeout;
+        $('.brand-nav').hover(function(){
+            $('.brand-nav').css("background","#fff");
+            $('.brand-nav a').css('color','#51216b');
+            $('.brand-nav a i').css('background','url(../img/integration.png) no-repeat -279px -185px');
+            clearTimeout(_timeout);
+            $('.brand').slideDown(250);
+        },function(){
+            _timeout=setTimeout(function(){
+                $('.brand').slideUp(250);
+                $('.brand-nav').css("background","#9f307c");
+                $('.brand-nav a').css({'color':'#fff','text-decoration': 'underline'});
+                $('.brand-nav a i').css('background','url(../img/integration.png) no-repeat -262px -185px');
+            },100)
+        });
+         
+        //内容也做hover事件
+        $(".brand").hover(function(){
+            clearTimeout(_timeout);
+            $('.brand').css("display","block");
+            $('.brand-nav').css("background","#fff");
+            $('.brand-nav a').css('color','#51216b');
+            $('.brand-nav a i').css('background','url(../img/integration.png) no-repeat -279px -185px');
+        },function(){
+            _timeout=setTimeout(function(){
+            $('.brand').slideUp(250);
+                $('.brand-nav').css("background","#9f307c");
+                $('.brand-nav a').css({'color':'#fff','text-decoration': 'underline'});
+                $('.brand-nav a i').css('background','url(../img/integration.png) no-repeat -262px -185px');
+            },100)
+        });
+         
+    });
+
+/*----------- 品牌导航end --------------*/
+    
+
+
+
+/*-------------- 倒计时 --------------*/
+
+       
+  
+     
+    window.show_time=function (){ 
+        var time_start = new Date().getTime(); //设定当前时间
+        var time_end =  new Date("2019/03/16 00:00:00").getTime(); //设定目标时间
+        // 计算时间差 
+        var time_distance = time_end - time_start; 
+       
+        // 时
+        var int_hour = Math.floor(time_distance/3600000) 
+        time_distance -= int_hour * 3600000; 
+        // 分
+        var int_minute = Math.floor(time_distance/60000) 
+        time_distance -= int_minute * 60000; 
+        // 秒 
+        var int_second = Math.floor(time_distance/1000) 
+        // 时分秒为单数时、前面加零 
+       
+        if(int_hour < 10){ 
+            int_hour = "0" + int_hour; 
+        } 
+        if(int_minute < 10){ 
+            int_minute = "0" + int_minute; 
+        } 
+        if(int_second < 10){
+            int_second = "0" + int_second; 
+        } 
+        // 显示时间 
+      
+        $(".hour").html(int_hour); 
+        $(".min").html(int_minute); 
+        $(".seconds").html(int_second); 
+        // 设置定时器
+        setTimeout("show_time()",1000); 
+       
+    } 
+    show_time();
